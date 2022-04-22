259aa3b2db67b0ebaaaafd978062fa2471caef18
itkachenko <nel18@yandex.ru>
2022-04-22 22:51:08 +0300

    Приложение работает

---
+++ src/main/java/com/example/demo/constant/ApplicationConstant.java
@@ -0,0 +1,8 @@
+package com.example.demo.constant;
+
+public class ApplicationConstant {
+    public static final String KAFKA_LOCAL_SERVER_CONFIG = "localhost:9092";
+    public static final String GROUP_ID_STRING = "group-id-string-1";
+    public static final String TOPIC_NAME = "myTopic";
+    public static final String KAFKA_LISTENER_CONTAINER_FACTORY = "kafkaListenerContainerFactory";
+}
